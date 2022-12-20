0a5e910a0373fe1fa66778b38dad34f5d66e56d1
Wouter Konings <wkonings@f0r3s16.codam.nl>
2022-12-20 08:22:05 +0100

    Changed keyhooks, minor change to intersections

--- src/intersections.c
+++ src/intersections.c
@@ -6,7 +6,7 @@
 /*   By: wkonings <wkonings@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/20 03:30:22 by wkonings      #+#    #+#                 */
-/*   Updated: 2022/12/20 07:57:12 by wkonings      ########   odam.nl         */
+/*   Updated: 2022/12/20 08:14:59 by wkonings      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ bool	hit_sphere(t_obj sphere, const t_ray *ray, t_inter *intersection)
 	
 	aligned = ray->origin - sphere.pos;
 	a = dot(ray->direction, ray->direction);
-	// b = 2.0 * dot(aligned, ray->direction);
-	b = dot(aligned, ray->direction);
+	b = 2.0 * dot(aligned, ray->direction);
+	// b = dot(aligned, ray->direction);
 	c = dot(aligned, aligned) - sqr(sphere.diameter / 2);
-	discriminant = b * b - a * c;
-	// discriminant = (b * b) - (4 * a * c);
+	// discriminant = b * b - a * c;
+	discriminant = (b * b) - (4 * a * c);
 	if (discriminant < 0.0f)
 		return (false);
 
@@ -37,9 +37,9 @@ bool	hit_sphere(t_obj sphere, const t_ray *ray, t_inter *intersection)
 	min = (-b - sqrt(b * b - a * c) / a);
 	plus = (-b + sqrt(b * b - a * c) / a);
 	//todo: get rid of tmp <= instead use tmp < ?
-	if (min > RAY_T_MIN && min < intersection->t)
+	if (min > RAY_T_MIN && min <= intersection->t)
 		intersection->t = min;
-	else if (plus > RAY_T_MIN && plus < intersection->t)
+	else if (plus > RAY_T_MIN && plus <= intersection->t)
 		intersection->t = plus;
 	else
 		return (false);
--- src/keyhooks.c
+++ src/keyhooks.c
@@ -6,7 +6,7 @@
 /*   By: wkonings <wkonings@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/14 18:46:37 by wkonings      #+#    #+#                 */
-/*   Updated: 2022/12/20 04:21:35 by wkonings      ########   odam.nl         */
+/*   Updated: 2022/12/20 08:20:56 by wkonings      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,19 @@ void	keyhook(mlx_key_data_t keydata, void	*param)
 
 	rt = param;
 	magnitude = 1;
+	if (mlx_is_key_down(rt->mlx, MLX_KEY_LEFT_SHIFT))
+		magnitude *= 10;
 	if (mlx_is_key_down(rt->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(rt->mlx);
 	if (keydata.action == MLX_RELEASE)
 		return ;
 	if (keydata.key == MLX_KEY_A)
-		rt->camera.pos[X] -= 1;
+		rt->camera.pos[X] -= 0.1;
 	if (keydata.key == MLX_KEY_D)
-		rt->camera.pos[X] += 1;
+		rt->camera.pos[X] += 0.1;
 	if (keydata.key == MLX_KEY_W)
-		rt->camera.pos[Y] -= 1;
+		rt->camera.pos[Y] -= 0.1;
 	if (keydata.key == MLX_KEY_S)
-		rt->camera.pos[Z] += 1;
-	// if (mlx_is_key_down(rt->mlx, MLX_KEY_LEFT_SHIFT))
-	// 	magnitude *= 10;
-	// transpose(rt, magnitude);
-	// scale(rt, magnitude);
-	// angle(rt, magnitude);
-	// setting_toggles(rt);
-	// if (rt->settings->scale < 1)
-	// 	rt->settings->scale = 1;
+		rt->camera.pos[Y] += 0.1;
 	cast_rays(rt);
 }
